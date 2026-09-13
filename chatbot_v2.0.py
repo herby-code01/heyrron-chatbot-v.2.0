3a3be2da54dcf7ba3ec8f9cea81d65fba65c2e9d
herby-code01 <sackeyherbert30@gmail.com>
2026-09-13 19:07:43 -0400

    updated codes

--- chatbot_v2.0.py
+++ chatbot_v2.0.py
@@ -1,6 +1,6 @@
 from google import genai as heyrronChat
 
-cli = heyrronChat.Client(api_key="A")
+cli = heyrronChat.Client(api_key="AQ")
 
 run = True
 exit = {'stop', 'quit','close','Q', 'Break'}
@@ -11,4 +11,9 @@ while run:
     if question.lower() in exit:
         run = False
 
+    initial_input = cli.models.generate_content(model="gemini-3.8-flash", contents="Start by saying I'm HeyrronChat version 2.0")    
+
     response = cli.models.generate_content(model ="gemini-3.8-flash", contents=question)
+
+    print(initial_input +"\n")
+    print("heyrronChat: ", response)
